67897a5debf8e8c6ab8999b949844c0ed0958c9f
Praneeth Reddy <penugondapr@gmail.com>
2023-05-18 21:15:49 +0530

    main page

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Front_end
+# AUTOMATED BOOK LOCATOR USING RASPBERRY PI - P PRANEETH REDDY, K VENKATA SATYA, B RADHA KRISHNA
 
 Front End For Raspberry pi Project
+
+In this project, a website that assists in finding the desired book will be developed. HTML, Bootstrap, PHP, jQuery, JavaScript, and Django will be used to build the website. To find a reference number, a database is searched for the book's title. The LED strips show the user the precise location of the book inside the shelf using a Raspberry Pi.
+
+This project will assist users in searching the books quickly with mobile devices and smartphones and can look or the necessary books without even touching the racks, as the technologies used previously for finding or searching the books arise some problems in searching books.
--- index.php
+++ index.php
@@ -1,85 +1,108 @@
+<?php include 'include/header.php'; ?>
 
-<?php include 'include/header.php';   ?>
-
-    <!-- section 1 -->
-    <div class="container mb-5 mt-5">
-        <div class="p-5 mb-4 text-bg-dark rounded-3">
-            <div class="d-flex flex-lg-nowrap flex-wrap text-center">
-            <div class="container-fluid py-5">
-              <h1 class="display-5 fw-bold ">Book Shelf Project</h1>
-              <p class="fs-4">
-                Find a Book with Light using PI
-              </p>
-            </div>
-            <div class="container-fluid">
-                <img src="assets/images/pi.jpg" alt="pi-img" class="img-fluid rounded-5" height="250rem" width="250rem">
-            </div>
-          </div>
-        </div>
-
-        <div id="about" class="container-fluid mt-5">
-            <div class="row">
-              <div class="col-sm-12" align="justify">
-                <h2 >About Project</h2><br>
-                <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <br><button class="btn btn-dark btn-lg">Project link <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
-              </div>
-            </div>
-          </div>
-        
-          <div class="container-fluid mt-5">
-            <h1 class="mb-3">Project Team</h1>
-            <div class="d-flex flex-lg-nowrap flex-md-nowrap flex-wrap">
-            <div class="col-md-4 me-3">
-                <div class="card mb-4 box-shadow">
-                  <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18554cc57b7%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18554cc57b7%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 me-3">
-                <div class="card mb-4 box-shadow">
-                  <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18554cc57b7%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18554cc57b7%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 me-3">
-                <div class="card mb-4 box-shadow">
-                  <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_18554cc57b7%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_18554cc57b7%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.828125%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-                  <div class="card-body">
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                      </div>
-                      <small class="text-muted">9 mins</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-        </div>
+<!-- section 1 -->
+<div class="container mb-5 mt-5">
+  <div class="p-5 mb-4 text-bg-dark rounded-3">
+    <div class="d-flex flex-lg-nowrap flex-wrap text-center">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold ">Automated Book Finder using Raspberry PI</h1>
+        <p class="fs-4">
+          Find a Book with Light using raspberry PI
+        </p>
+      </div>
+      <div class="container-fluid">
+        <img src="assets/images/pi.jpg" alt="pi-img" class="img-fluid rounded-5" height="250rem" width="250rem">
+      </div>
     </div>
+  </div>
+
+  <div id="about" class="container-fluid mt-5">
+    <div class="row">
+      <div class="col-sm-12" align="justify">
+        <h2>About Project</h2><br>
+        <p>In this project, a website that assists in finding the desired book will be developed. HTML, Bootstrap, PHP,
+          jQuery, JavaScript, and Django will be used to build the website. To find a reference number, a database is
+          searched for the book's title. The LED strips show the user the precise location of the book inside the shelf
+          using a Raspberry Pi.
+          This project will assist users in searching the books quickly with mobile devices and smartphones and can look
+          for the necessary books without even touching the racks, as the
+          technologies used previously for finding or searching the books arise some problems in searching books.</p>
+        <br><button class="btn btn-dark btn-lg" onclick="window.location.href = 'https://github.com/PenugondaPraneethReddy/Automated-Book-Finder'">Project link <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-5">
+    <h1 class="mb-3">Project Team</h1>
+    <div class="d-flex flex-lg-nowrap flex-md-nowrap flex-wrap">
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/Praneeth.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+          <h4>P Praneeth Reddy (19BCI0154)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/satya.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+            <h4>K Venkata Satya (19BCI0142)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 me-3">
+        <div class="card mb-4 box-shadow">
+          <img class="card-img-top"
+            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+            alt="Thumbnail [100%x225]" style="height: 350px; width: 100%; display: block;"
+            src="assets/images/RadhaKrishna.jpeg"
+            data-holder-rendered="true">
+          <div class="card-body">
+          <h4>B Radha Krishna (19BCT0255)</h4>
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="btn-group">
+                <button type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-person"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-instagram"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-linkedin"></i></button>
+                <button type="button" class="btn btn-sm btn-outline-secondary" ><i class="fa-brands fa-github"></i></button>
+                
+              </div>
+              <small class="text-muted"></small>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
 
-    
-  <?php include 'include/footer.php' ?>
+
+<?php include 'include/footer.php' ?>
